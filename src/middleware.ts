ceec106826bd41c8ebee2290bfa9d87fea562121
Titux Metal (DEV) <github@lgdweb.fr>
2024-10-18 18:58:44 +0200

    feat: Refactor middleware to include authentication and database sync
    - Introduced `dbSync` middleware to synchronize the database with Turso in development mode.
    - Added `auth` middleware to handle session validation and user authentication.
    - Replaced `defineMiddleware` with `sequence` to manage multiple middleware functions.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,49 @@
-import { defineMiddleware } from 'astro:middleware'
+import type { APIContext, MiddlewareNext } from 'astro'
+import { sequence } from 'astro:middleware'
+import { lucia } from './lib/auth'
 import { libsql } from './lib/prisma'
 
-export const onRequest = defineMiddleware(async (context, next) => {
-  console.log('Sync database with Turso')
-
-  await libsql.sync()
+const dbSync = async (_: unknown, next: MiddlewareNext) => {
+  if (import.meta.env.DEV) {
+    console.log('Sync database with Turso')
+    await libsql.sync()
+  }
 
   return next()
-})
+}
+
+const auth = async (context: APIContext, next: MiddlewareNext) => {
+  const sessionId = context.cookies.get(lucia.sessionCookieName)?.value ?? null
+  console.log('middleware sessionId', sessionId)
+
+  if (!sessionId) {
+    context.locals.user = null
+    context.locals.session = null
+
+    return next()
+  }
+
+  const { session, user } = await lucia.validateSession(sessionId)
+  console.log('middleware session', session)
+  if (session && session.fresh) {
+    const sessionCookie = lucia.createSessionCookie(session.id)
+
+    context.cookies.set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes)
+  }
+
+  if (!session) {
+    const sessionCookie = lucia.createBlankSessionCookie()
+
+    context.cookies.set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes)
+  }
+
+  context.locals.session = session
+  context.locals.user = user
+
+  console.log('middleware local session', context.locals.session)
+  console.log('middleware local user', context.locals.user)
+
+  return next()
+}
+
+export const onRequest = sequence(dbSync, auth)
